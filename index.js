2638998935b625bb00d06692166cd8b741ae0990
Andrew Stoughton <andrew.stoughton@greenlightcommerce.com>
2020-03-02 12:09:38 +0000

    For Loops in Vue

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      seen: true
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
     }
-  })
+  });
   
-  app.seen = false;
+  app.todos.push({ text: 'New item' })
