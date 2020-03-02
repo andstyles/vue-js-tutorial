1639bf4953e8bed6aa1e5f9a8d26016e2bf81bbe
Andrew Stoughton <andrew.stoughton@greenlightcommerce.com>
2020-03-02 12:16:24 +0000

    Components

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
+Vue.component('todo-item', {
+    template: '<li>This is a todo</li>'
+  })
+  
+  var app = new Vue({
+    el: '#app'
   })
