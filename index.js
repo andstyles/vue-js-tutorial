330c8759f57b7761ebd3ec6bfaab1fa524891f86
Andrew Stoughton <andrew.stoughton@greenlightcommerce.com>
2020-03-02 12:22:37 +0000

    Create Components With Props

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 Vue.component('todo-item', {
-    template: '<li>This is a todo</li>'
-  })
-  
-  var app = new Vue({
-    el: '#app'
-  })
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+})
